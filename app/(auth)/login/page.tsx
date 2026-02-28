21ed9c007778e6e57242b6f0f6bb6c0905b38810
worker <work@yaojiaus-MacBook-Pro.local>
2026-02-28 09:13:25 +0800

    1

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,22 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import { redirect, useRouter } from "next/navigation"
+import Galaxy from '../../../components/ui/Galaxy';
 export default function LoginPage() {
-  return <div>登录</div>
+    const router = useRouter()
+
+    function handleregister() {
+        // 跳转到登录页面
+        router.push('/register')
+    }
+    return <div className="w-full top-0 left-0  h-screen relative bg-black">
+        <div className="absolute inset-0">
+            <Galaxy transparent={false} />
+        </div>
+
+        <div className="relative z-10">
+            
+        </div>
+
+    </div>
 }
