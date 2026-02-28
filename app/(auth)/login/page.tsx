a1a595978888e5ccd4e0347885320ceb4d25524d
worker <work@yaojiaus-MacBook-Pro.local>
2026-02-28 09:44:33 +0800

    1

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { Button } from "@/components/ui/button"
+import ASCIIText  from "../../../components/ASCIIText"
 import { redirect, useRouter } from "next/navigation"
 import Galaxy from '../../../components/ui/Galaxy';
 export default function LoginPage() {
     const router = useRouter()
 
     function handleregister() {
-        // 跳转到登录页面
+        // 跳转到注册页面
         router.push('/register')
     }
     return <div className="w-full top-0 left-0  h-screen relative bg-black">
@@ -15,7 +16,15 @@ export default function LoginPage() {
         </div>
 
         <div className="relative z-10">
-            
+            <div className="">
+                <ASCIIText
+                    enableWaves
+                    asciiFontSize={4}
+                    text="欢迎来到盐焗虾的博客"
+                    textColor="#fefae0"
+                   
+                />
+            </div>
         </div>
 
     </div>
